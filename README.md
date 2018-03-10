1d1f29bd221e42d370b9f37d47a2c77b4719d401
Jan Černý <j.cerny@gmc.net>
2018-03-10 21:00:19 +0100

    bumped version 1.5; fixed bug - Does not work in "Use experimental version of full page issue view by default"

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
 # YouTrack Wider
 This add-on injects JavaScript into YouTrack web pages for wider view. 
 
-Compatible with YouTrack *2017.4 Build 39533* and lower.
+Compatible with YouTrack *2018.1 Build 39916* and lower.
 
 Before add-on: 
 ![before](Before.png)
+![before](Before2.png)
+![before](Before3.png)
 
 After add-on:
 ![after](After.png)
+![after](After2.png)
+![after](After3.png)
 
 ## Release Notes
+### v1.5
+* about 50% bigger field for edit Saved search
+* added support for new (experimental) theme
 ### v1.4
 * minor bug fix
 * size reduction
 
 ## Add-on for FireFox
-[YouTrack Wider v1.4](https://addons.mozilla.org/cs/firefox/addon/youtrack-wider/)
+[YouTrack Wider v1.5](https://addons.mozilla.org/cs/firefox/addon/youtrack-wider/)
 
 ## Add-on for Chrome
-* download file [YouTrack Wider v1.4](https://github.com/cernyjan/YouTrack-Wider/blob/master/youtrack_wider-1.4-an+fx.xpi)
+* download file [YouTrack Wider v1.5](https://github.com/cernyjan/YouTrack-Wider/blob/master/youtrack_wider-1.5-an+fx.xpi)
 * extract it on the file system
 * open Extensions page in browser
 * click the “Developer mode” checkbox to activate it
--- YouTrack-Wider.js
+++ YouTrack-Wider.js
@@ -1,29 +1,82 @@
-$('.ring-header-react.ring-header-react_issues.ng-scope').css({ 'max-width': '100%' });
-$('#id_l\\.I\\.bigPanel').css({ 'max-width': '100%' });
-$('#id_l\\.I\\.mainPanel').css({ 'max-width': '100%' });
+$( document ).ready(function() {
+	console.log("YouTrack Wider is Online");
 
-window.addEventListener('popstate', function(e){
-	var currenthash = window.location.hash;
-	if (currenthash.indexOf("#newissue") >= 0 || currenthash.indexOf("&newissue") >= 0) 
-	{
-		$('#id_l\\.I\\.ni\\.newIssuePane').css({ 'max-width': '100%' });
+	function oldTheme() {
+		$('.ring-header-react.ring-header-react_issues.ng-scope').css({ 'max-width': '100%' });
+		$('#id_l\\.I\\.bigPanel').css({ 'max-width': '100%' });
+		$('#id_l\\.I\\.mainPanel').css({ 'max-width': '100%' });
+
+		window.addEventListener('popstate', function(e){
+			var currenthash = window.location.hash;
+			if (currenthash.indexOf("#newissue") >= 0 || currenthash.indexOf("&newissue") >= 0) 
+			{
+				$('#id_l\\.I\\.ni\\.newIssuePane').css({ 'max-width': '100%' });
+			}
+		})
+
+		var urlParams = new URLSearchParams(window.location.search);
+		if (urlParams.has('draftId'))
+		{
+			$('.container_width.rootNewIssue').css({ 'max-width': '100%' });
+			$('#id_l\\.N\\.ni\\.newIssuePane').css({
+		   	'padding-left': '32px',
+		   	'padding-right': '32px',
+		   	'max-width': '100%'
+			});
+			$('.footer-placeholder.footer-placeholder_right').css({ 'padding-left': '32px' });
+		} 
+
+		$( "#id_l\\.I\\.sb\\.filters_div" ).css( "width", "298px" );
+		$('.vert-list-wrp').css({ 'width': '255px' });
+
+		$('.center.issues-center').css({ 'margin-left': '316px' });
+		$('.default-layout.container.container_width.container_padding').css({ 'max-width': '100%' });
+
+		function iteration() {
+			myVar = setInterval(checkDialogForSavedSearch, 1000);
+		}
+
+		function checkDialogForSavedSearch() {
+			if ($('div#id_l\\.I\\.sb\\.s\\.ewfd\\.editWatchFolderDlg').length > 0)
+			{
+				if ($('div#id_l\\.I\\.sb\\.s\\.ewfd\\.editWatchFolderDlg[style*="width: 100%;"]').length == 0)
+				{
+					$('#id_l\\.I\\.sb\\.s\\.ewfd\\.searchField').css({ 'width': '100%' });
+					$('#id_l\\.I\\.sb\\.s\\.ewfd\\.watchFolderName').css({ 'width': '100%' });
+				}			
+			}		    
+		}
+
+		iteration();
 	}
-})
 
-var urlParams = new URLSearchParams(window.location.search);
-if (urlParams.has('draftId'))
-{
-	$('.container_width.rootNewIssue').css({ 'max-width': '100%' });
-	$('#id_l\\.N\\.ni\\.newIssuePane').css({
-   	'padding-left': '32px',
-   	'padding-right': '32px',
-   	'max-width': '100%'
-	});
-	$('.footer-placeholder.footer-placeholder_right').css({ 'padding-left': '32px' });
-} 
+	var app = "";
+	var loop = 1;
 
-$( "#id_l\\.I\\.sb\\.filters_div" ).css( "width", "298px" );
-$('.vert-list-wrp').css({ 'width': '255px' });
+	var appExists = setInterval(function() {
+		if (loop > 3)
+		{
+			clearInterval(appExists);
+			oldTheme();
+		}
+		else
+		{
+			app = $('.app__container');
+	 		if ($(app).length) {
+	    		clearInterval(appExists);
+	    		$(app).css({ 'max-width': '100%' });
+				$( "div[data-test='wikiText']" ).css({ 'max-width': '100%' });
+	 		}
+	 		else
+	 		{
+		 		if ( $( "div[baseuri='/']" ).length )
+		 		{
+		 			clearInterval(appExists);
+					oldTheme();
+		 		}
+		 	}
 
-$('.center.issues-center').css({ 'margin-left': '316px' });
-$('.default-layout.container.container_width.container_padding').css({ 'max-width': '100%' });
+	 		loop = loop + 1;
+	 	}
+	}, 100);
+});
